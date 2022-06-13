8c7b994d2bc3ff0036703292888740e3ef6d58c8
ChangDae1993 <jsj939639@gmail.com>
2022-06-13 16:47:55 +0900

    Enemy_원거리 공격 구현 Idea
    원거리 공격시 일정 범위 내에 들어오면 화살 공격 실행
    일정 거리보다 가까워지면 플레이어 key값을 받아서 그 키값 * 이동속도만큼 플레이어로부터 멀어짐 실행(공격 끝내고 도망치는 것)

--- Assets/03.Scripts/03.InGame_Scene/Enemy/Enemy_State_Ctrlr.cs
+++ Assets/03.Scripts/03.InGame_Scene/Enemy/Enemy_State_Ctrlr.cs
@@ -23,4 +23,8 @@ public class Enemy_State_Ctrlr : MonoBehaviour
     {
         e_State = EnemyState.enemy_idle;
     }
+
+    ///원거리 공격시 일정 범위 내에 들어오면 화살 공격 실행
+    ///일정 거리보다 가까워지면 플레이어 key값을 받아서 그 키값 * 이동속도만큼 플레이어로부터 멀어짐 실행
+    ///(공격 끝내고 도망치는 것)
 }
