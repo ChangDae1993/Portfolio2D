cae9c02936703a511464630848d277a582afeeb6
ChangDae1993 <jsj939639@gmail.com>
2022-07-23 15:06:45 +0900

    Hook_Return_MODIFY_Ver.1

--- Assets/03.Scripts/03.InGame_Scene/Player/Player_Attack/Hook/Hook_Ctrl.cs
+++ Assets/03.Scripts/03.InGame_Scene/Player/Player_Attack/Hook/Hook_Ctrl.cs
@@ -6,9 +6,10 @@ public class Hook_Ctrl : MonoBehaviour
 {
     private Hook_Aim_Ctrl aim_Ctrl;
     private float hook_speed;
-    private Vector3 target_Vec;
-    private float target_Dist;
+    //private Vector3 target_Vec;
+    //private float target_Dist;
     private Transform player_Pos;
+    private float hook_back_Dist;
 
     private void Start() => StartFunc();
 
@@ -17,22 +18,35 @@ public class Hook_Ctrl : MonoBehaviour
         aim_Ctrl = GameObject.FindGameObjectWithTag("Player").GetComponent<Hook_Aim_Ctrl>();
         hook_speed = 6.0f;
         player_Pos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        hook_back_Dist = 0.0f;
     }
 
     private void Update() => UpdateFunc();
 
     private void UpdateFunc()
     {
-        //aim_Ctrl.hook_Target_Pos를 목적지로 본인 위치부터 hook_Target_Pos까지 이동
-        this.transform.position = Vector3.Lerp(this.transform.position, aim_Ctrl.hook_Target_Pos, Time.deltaTime * hook_speed);
-        
-        //aim이랑 일정 거리 가까워 졌을 때 돌아오기 구현
-        //target_Vec = aim_Ctrl.hook_Target_Pos - this.transform.position;
-        //target_Dist = Vector3.Magnitude(target_Vec);
-
+        if(hook_back_Dist <= 0.01f)
+        {
+            Hook_Return();
+        }
         //if (0.5f <= target_Dist)
         //{
         //    this.transform.position = Vector3.Lerp(this.transform.position, player_Pos.position, Time.deltaTime * hook_speed);
         //}
+
+        //aim_Ctrl.hook_Target_Pos를 목적지로 본인 위치부터 hook_Target_Pos까지 이동
+        this.transform.position = Vector3.Lerp(this.transform.position, aim_Ctrl.hook_Target_Pos, Time.deltaTime * hook_speed);
+        hook_back_Dist = Mathf.Abs(this.transform.position.x - aim_Ctrl.hook_Target_Pos.x);
+        //aim이랑 일정 거리 가까워 졌을 때 돌아오기 구현
+        //target_Vec = aim_Ctrl.hook_Target_Pos - this.transform.position;
+        //target_Dist = Vector3.Magnitude(target_Vec);
     }
+
+    private void Hook_Return()
+    {
+        Debug.Log("back");
+        this.transform.position = Vector3.Lerp(this.transform.position, player_Pos.position, Time.deltaTime * hook_speed);
+    }
+
+
 }
