4c601724fcac3a3716618e40282e7631a22cb405
ChangDae1993 <jsj939639@gmail.com>
2022-08-01 19:10:27 +0900

    Title_Scene_ADD + UI_Img_ADD

---
+++ Assets/03.Scripts/01.Title_Scene/Title_Mgr.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Title_Mgr : MonoBehaviour
+{
+    public Button Start_Btn;
+    public Image fadeOut;
+
+    private void Start() => StartFunc();
+
+    private void StartFunc()
+    {
+         
+    }
+
+    private void Update() => UpdateFunc();
+
+    private void UpdateFunc()
+    {
+        
+    }
+}
