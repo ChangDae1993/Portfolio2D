bc9c83a0e37c46c7e168b7058558b27727599905
ChangDae1993 <jsj939639@gmail.com>
2022-08-21 12:33:08 +0900

    Effect_ADD_ING

--- Assets/03.Scripts/03.InGame_Scene/Player/Player_Attack/Player_Skill/Skill2.cs
+++ Assets/03.Scripts/03.InGame_Scene/Player/Player_Attack/Player_Skill/Skill2.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Skill2 : MonoBehaviour
 {
+    public Image chargeImgBG;
+    public Image chargeImg;
+
+    public GameObject shotPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        chargeImgBG.gameObject.SetActive(false);
+        shotPos.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            chargeImgBG.gameObject.SetActive(true);
+            shotPos.gameObject.SetActive(true);
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            chargeImgBG.gameObject.SetActive(false);
+            shotPos.gameObject.SetActive(false);
+        }
+        else if(Input.GetMouseButton(0))
+        {
+            chargeImgBG.gameObject.SetActive(false);
+            shotPos.gameObject.SetActive(false);
+        }
     }
 }
