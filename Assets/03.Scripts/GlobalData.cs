16c1502e720b3ee508a209b84275f640d8ecffaf
ChangDae1993 <jsj939639@gmail.com>
2022-09-02 12:05:16 +0900

    GlobalData_ADD

---
+++ Assets/03.Scripts/GlobalData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalData : MonoBehaviour
+{
+
+    public static int hpPotionNum;
+
+    public static float playerExp;
+
+
+    private void Start() => StartFunc();
+    private void StartFunc()
+    {
+        hpPotionNum = 5;
+        playerExp = 0.0f;
+    }
+
+    private void Update() => UpdateFunc();
+    private void UpdateFunc()
+    {
+        
+    }
+}
